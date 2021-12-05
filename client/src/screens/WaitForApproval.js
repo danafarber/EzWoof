0518af37dc809445239b2d87c27162403644c0ab
danafarber <danafarber39@gmail.com>
2021-12-05 19:51:25 +0200

    trainer pripfile and approval screen

---
+++ client/src/screens/WaitForApproval.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function WaitForApproval()
+{
+ return(
+    <div data-testid="waitForApproval" className="main">
+        <img src="../assets/waitApproval.png" ></img>
+        <h1>היי זלמה! </h1>
+        <p className="content"> 
+        
+        תודה שנרשמת לשירות!
+        כחלק מהמודרציה המופעלת באפליקציה,
+        אנו עוברים על כל הפרטים ומאשרים בהתאם.
+         אנא המתן למייל.
+        </p>
+
+    </div>
+  
+ )
+
+
+
+}
+
+export default WaitForApproval;
